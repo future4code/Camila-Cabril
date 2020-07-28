c6126db87c81e140259b7fa88d233621df939cc8
Carolina Cabril <carolina-cabril@hotmail.com>
2020-07-28 13:30:56 -0300

    criação dos exercicios

---
+++ semana3/operadores/index.js
@@ -0,0 +1,64 @@
+// Exercícios de interpretação de código
+
+// 1- a.  false
+//    b.  false
+//    c.  true
+//    e.  boolean
+
+// 2- a.  undefined
+//    b.  null
+//    c.  11
+//    d.  3
+//    e.  [3, 19, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+//    f.  9
+
+
+
+//Exercícios de escrita de código
+
+// Exercício 1
+// const idade = prompt('Qual a sua idade ?')
+// const idadeAmigo = prompt('Qual a idade do seu melhor amigo ou da sua melhor amiga ?')
+
+// const calculo = Number(idade) - Number(idadeAmigo)
+
+// console.log('Sua idade é maior do que a do seu melhor amig@?', Number(idade) > Number(idadeAmigo))
+
+// console.log('Diferença de idade: ' + calculo + ' anos')
+
+
+
+// Exercício 2
+// const par = Number(prompt('Insira um número par'))
+// console.log(par%2)
+
+//Notei que todos os números pares o resto da divisão é 0.
+//No caso dos números ímpares o resto da divisão é 1.
+
+
+// Exercício 3
+// let listaDeTarefas = []
+
+// const tarefa1 = prompt('Digite a primeira tarefa ?')
+// const tarefa2 = prompt('Digite a segunda tarefa ?')
+// const tarefa3 = prompt('Digite a terceira tarefa ?')
+
+// listaDeTarefas.push(tarefa1)
+// listaDeTarefas.push(tarefa2)
+// listaDeTarefas.push(tarefa3)
+
+// const tarefaFeita = Number(prompt('Digite o índice de uma tarefa que ele já realizou: 0, 1 ou 2 '))
+
+// console.log(listaDeTarefas)
+
+// listaDeTarefas.splice(tarefaFeita, 1)
+
+// console.log(listaDeTarefas)
+
+
+// Exercício 4
+// const nomeDoUsuario = prompt('Qual seu nome ?')
+// const emailDoUsuario = prompt('Qual seu e-mail ?')
+
+// console.log('O e-mail ' + emailDoUsuario + ' foi cadastrado com sucesso. Seja bem-vinda(o), ' + nomeDoUsuario + '!')
+
