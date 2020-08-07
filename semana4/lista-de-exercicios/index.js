4e96e8bec9799dbf57eab6d64e41b549265b9bcf
Carolina Cabril <carolina-cabril@hotmail.com>
2020-08-07 11:44:29 -0300

    exercicio conversorDeMoedas

---
+++ semana4/lista-de-exercicios/index.js
@@ -0,0 +1,5 @@
+// Exercícios de interpretação de código
+
+// 1.
+// A função conversorDeMoedas, recebe como pararametro o valor em dolar, dentro da função é solicitado para o usuário qual o valor da cotação e ela retorna o valor do dolar vezes a cotação. A constante meuDinhero recebe o valor retonado da função e o console imprime esse valor. Usei o valor da cotção R$5 ENTÃO 5 * 100 = 500
+
