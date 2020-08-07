9e97a72dac45aac9f27dcaaf3bb68ccd696ce03f
Carolina Cabril <carolina-cabril@hotmail.com>
2020-08-07 13:05:06 -0300

    for

--- semana4/lista-de-exercicios/index.js
+++ semana4/lista-de-exercicios/index.js
@@ -3,3 +3,15 @@
 // 1.
 // A função conversorDeMoedas, recebe como pararametro o valor em dolar, dentro da função é solicitado para o usuário qual o valor da cotação e ela retorna o valor do dolar vezes a cotação. A constante meuDinhero recebe o valor retonado da função e o console imprime esse valor. Usei o valor da cotção R$5 ENTÃO 5 * 100 = 500
 
+
+
+// 2.
+// A função investeDinheiro recebe dois parametros, que são eles tipoDeInvestimento e valor, dentro da função a variável valorAposInvetimento é declarada sem nenhuma atribuição. O switch faz uma verificação do tipo de investimento e para casa tipo ele possui uma regra, como SE tipoDeInvestimento case "Poupança" a variavel valorAposInvetimento é igaul ao valor * 1.03 e assim para cada caso. O primeiro console imprime 165 e o segundo tipo de investimento informado incorreto, pois não possui regra para esse tipo de investimento.
+
+
+// 3.
+// O for percorre a array numeros e faz um verificação se o valor da posição atual divido por 2 o resto da divisão for igual a 0 o array1 recebe esse valor, caso contrario o array2 recebe o valor. O primeiro console imprime a frase e quantidade de itens que o array numero possui no caso 14. O segundo console imprime a quantidade de itens do array1 no caso 5. E o ultimo console imprime a quantidade de itens no array2 no caso 9.
+
+
+
+// 4.
